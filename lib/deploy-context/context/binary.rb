75cedaeed35a5347eabce990359141f7907a728b
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2023-10-03 00:15:12 -0400

    Create deploy-context automatic commit

--- lib/deploy-context/context/binary.rb
+++ lib/deploy-context/context/binary.rb
@@ -6,22 +6,28 @@ def deployer
   DEPLOYER
 end
 
+puts "Parameter pass #{ARGV[0]}"
+
 if ARGV[0].nil?
   deployer.cycle
 else
   case ARGV[0]
   when 'once'
-    puts "Parameter passe #{ARGV[0]}"
+    puts "\nExecute only once\n"
     deployer.cycle
   when 'always'
+    puts "\nAlways in execution\n"
     while true do
       deployer.cycle
     end
   when 'bump'
+    puts "\nBump minor version\n"
     deployer.minor_bump
   when 'release'
+    puts "\nBump major version\n"
     deployer.major_bump
   when 'test'
+    puts "\nExecute tests\n"
     deployer.cucumber_test(deployer)
   else
     puts "Unkonown setting #{ARGV[0]}"
