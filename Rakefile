7510e2d47d77526885989d85cced6ca20f1acf99
Jimmy Provencher <jimmy.provencher@cgi.com>
2023-10-04 03:08:19 -0400

    Create deploy-context automatic commit

--- Rakefile
+++ Rakefile
@@ -1,16 +1,3 @@
-require 'rubygems'
-require 'bundler'
+require_relative 'lib/deploy-context/helpers/rake_tasks'
 
-Bundler::GemHelper.install_tasks
-
-require 'rdoc/task'
-
-Rake::RDocTask.new do |rd|
-	rd.main = "README.md"
-	rd.title = 'deploy-context'
-	rd.rdoc_files.include("README.md", "lib/**/*.rb")
-end
-
-require 'git-version-bump/rake-tasks'
-
-require_relative 'lib/deploy-context/helpers/rake'
+task :default => "deploycontext:default"
---
+++ lib/deploy-context/helpers/rake_tasks.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler::GemHelper.install_tasks
+
+require 'rdoc/task'
+
+Rake::RDocTask.new do |rd|
+	rd.main = "README.md"
+	rd.title = 'deploy-context'
+	rd.rdoc_files.include("README.md", "lib/**/*.rb")
+end
+
+require 'git-version-bump/rake-tasks'
+
+require_relative '../../deploy-context'
+
+namespace :deploycontext do
+  deployer = Context::DeployContext.deployer
+
+  task :default => "deploycontext:test" do
+    deployer.execute_action(deployer, 'once')
+  end
+
+  task :bump do
+    deployer.execute_action(deployer, 'bump')
+  end
+
+  task :test do
+    deployer.execute_action(deployer, 'test')
+  end
+end
