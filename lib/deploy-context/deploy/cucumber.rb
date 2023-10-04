d016e6523e3a4144eeeecf42474a5cc8ffc3162a
Jimmy Provencher <jimmy.provencher@cgi.com>
2023-10-04 01:20:16 -0400

    Create deploy-context automatic commit

--- lib/deploy-context/deploy/cucumber.rb
+++ lib/deploy-context/deploy/cucumber.rb
@@ -2,9 +2,9 @@ module Context
   module DeployHelpers
     module CucumberHelper
       def cucumber_test(context)
-        git_build(context)
+        context.git_build(context)
         puts "Working in folder #{Dir.pwd}\nAnd context #{context.context_name} is created in folder #{context.context_folder} at version #{context.version}"
-        cucumber
+        context.cucumber(context)
       end
     end
   end
