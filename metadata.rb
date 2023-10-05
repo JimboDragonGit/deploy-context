299e50a03a998eaf03d49eabd251d9a9eb97cec4
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2023-10-04 21:48:03 -0400

    Create deploy-context automatic commit

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer 'Jimmy Provencher'
 maintainer_email 'jimmy.provencher@hotmail.ca'
 license 'All Rights Reserved'
 description 'Installs/Configures deploy-context'
-version "#{GVB.major_version}.#{GVB.minor_version}.#{GVB.patch_version}"
+version "#{GVB.major_version(true)}.#{GVB.minor_version(true)}.#{GVB.patch_version(true)}"
 chef_version '>= 16.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
