92f9e8ac427d9fc1d9ae151fe615d135b7792a22
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2023-10-04 22:28:01 -0400

    Create deploy-context automatic commit

--- libraries/deploy-context/deploy.rb
+++ libraries/deploy-context/deploy.rb
@@ -1,6 +1,7 @@
 require_relative 'helpers/command'
 require_relative 'deploy/deployer'
 require_relative 'deploy/git'
+require_relative 'deploy/context'
 
 
 module Context
@@ -8,6 +9,7 @@ module Context
     include CommandHelper
     include DeployHelpers::DeployerHelper
     include DeployHelpers::GitHelper
+    include DeployHelpers::ContextHelper
 
     attr_reader :context_name
     attr_reader :context_folder
@@ -15,107 +17,6 @@ module Context
     def initialize(context_name, deploycontext_folder)
       @context_name = context_name
       @context_folder = move_folder(deploycontext_folder)
-    end
-
-    def present_localy?
-      Dir.exist?(context_folder)
-    end
-
-    def actual_working_directory?
-      Dir.pwd == context_folder
-    end
-
-    def move_folder(folder)
-      @context_folder = folder.include?(context_name) ? folder : File.join(folder, context_name)
-    end
-
-    def check_folder(folder)
-      FileUtils.mkdir_p(context_folder) unless present_localy?
-    end
-
-    def version
-      git_build(self)
-      log "Getting version info for #{context_folder} and version should be #{GitVersionBump.version(true)}"
-      Gem::Version.new(GitVersionBump.version(true))
-    end
-
-    def cycle
-      ruby_cycle(self)
-    end
-
-    def test_context_successful?
-      log "Check if #{context_name} is install #{version}"
-      if gem_installed?(self)
-        log "Test context #{context_name} was successfully install on version #{version}"
-        true
-      else
-        log "Test context #{context_name} has failed to install #{version}"
-        false
-      end
-      cookbook_test(self)
-    end
-
-    def build
-      ruby_build(self)
-      cookbook_build(self)
-      check_folder get_context_file(self, 'contexts')
-    end
-
-    def commit
-      git_commit(self)
-      cookbook_install(self)
-    end
-
-    def release
-      log "\n\nRelease #{context_name} at version #{version}"
-      cookbook_push(self)
-      ruby_release(self)
-      git_release(self)
-    end
-
-    def install
-      ruby_install(self)
-    end
-
-    def clean
-      clean_folder(self, 'contexts')
-      ruby_clean(self)
-      cookbook_clean(self)
-    end
-
-    def show_new_version(level)
-      log "#{level} bump #{context_name} at newer version #{version}"
-    end
-
-    def patch_bump
-      GitVersionBump.tag_version("#{GitVersionBump.major_version(true)}.#{GitVersionBump.minor_version(true)}.#{GitVersionBump.patch_version(true) + 1}")
-      show_new_version('Patch')
-    end
-
-    def minor_bump
-      GitVersionBump.tag_version("#{GitVersionBump.major_version(true)}.#{GitVersionBump.minor_version(true) + 1}.0")
-      show_new_version('Minor')
-    end
-
-    def major_bump
-      GitVersionBump.tag_version("#{GitVersionBump.major_version(true) + 1}.0.0")
-      show_new_version('Major')
-    end
-
-    def wait_until_release_available
-      wait_until_release_available unless is_present_publicly?
-    end
-
-    def is_present_publicly?
-      ruby_check_if_available_public(self)
-    end
-
-    def new_update_available?
-      git_update_available?(self)
-    end
-
-    def ready_for_major_update?
-      false
     end
   end
 end
---
+++ libraries/deploy-context/deploy/context.rb
@@ -0,0 +1,106 @@
+module Context
+  module DeployHelpers
+    module ContextHelper
+      def present_localy?
+        Dir.exist?(context_folder)
+      end
+  
+      def actual_working_directory?
+        Dir.pwd == context_folder
+      end
+  
+      def move_folder(folder)
+        @context_folder = folder.include?(context_name) ? folder : File.join(folder, context_name)
+      end
+  
+      def check_folder(folder)
+        FileUtils.mkdir_p(context_folder) unless present_localy?
+      end
+  
+      def version
+        git_build(self)
+        log "Getting version info for #{context_folder} and version should be #{GitVersionBump.version(true)}"
+        Gem::Version.new(GitVersionBump.version(true))
+      end
+  
+      def cycle
+        ruby_cycle(self)
+      end
+  
+      def test_context_successful?
+        log "Check if #{context_name} is install #{version}"
+        if gem_installed?(self)
+          log "Test context #{context_name} was successfully install on version #{version}"
+          true
+        else
+          log "Test context #{context_name} has failed to install #{version}"
+          false
+        end
+        cookbook_test(self)
+      end
+  
+      def build
+        ruby_build(self)
+        cookbook_build(self)
+        check_folder get_context_file(self, 'contexts')
+      end
+  
+      def commit
+        git_commit(self)
+        cookbook_install(self)
+      end
+  
+      def release
+        log "\n\nRelease #{context_name} at version #{version}"
+        cookbook_push(self)
+        ruby_release(self)
+        git_release(self)
+      end
+  
+      def install
+        ruby_install(self)
+      end
+  
+      def clean
+        clean_folder(self, 'contexts')
+        ruby_clean(self)
+        cookbook_clean(self)
+      end
+  
+      def show_new_version(level)
+        log "#{level} bump #{context_name} at newer version #{version}"
+      end
+  
+      def patch_bump
+        GitVersionBump.tag_version("#{GitVersionBump.major_version(true)}.#{GitVersionBump.minor_version(true)}.#{GitVersionBump.patch_version(true) + 1}")
+        show_new_version('Patch')
+      end
+  
+      def minor_bump
+        GitVersionBump.tag_version("#{GitVersionBump.major_version(true)}.#{GitVersionBump.minor_version(true) + 1}.0")
+        show_new_version('Minor')
+      end
+  
+      def major_bump
+        GitVersionBump.tag_version("#{GitVersionBump.major_version(true) + 1}.0.0")
+        show_new_version('Major')
+      end
+  
+      def wait_until_release_available
+        wait_until_release_available unless is_present_publicly?
+      end
+  
+      def is_present_publicly?
+        ruby_check_if_available_public(self)
+      end
+  
+      def new_update_available?
+        git_update_available?(self)
+      end
+  
+      def ready_for_major_update?
+        false
+      end
+    end
+  end
+end
