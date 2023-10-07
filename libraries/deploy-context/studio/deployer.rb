4577cc6f98265a9ba5f76fcdb796d27d62eabbc9
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2023-10-07 03:51:59 -0400

    Create deploy-context automatic commit

--- libraries/deploy-context/studio/deployer.rb
+++ libraries/deploy-context/studio/deployer.rb
@@ -7,12 +7,12 @@ module Context
       end
 
       def self.method_missing(method_name, *argv, &block)
-        puts "Searching method #{method_name} in context #{@deployer}"
+        @deployer.debug_log "Searching method #{method_name} in context #{@deployer}"
         is_deploy_context_respond_to_action = @deployer.respond_to?(method_name) && @deployer.actions_permitted?(method_name.to_s)
         if is_deploy_context_respond_to_action
           @deployer.execute_action(@deployer, method_name)
         else
-          puts "\n\nUnavailable action '#{method_name}' for context #{@deployer.context_name} as #{@deployer.respond_to?(method_name)} and #{@deployer.actions_permitted?(method_name)} for #{@deployer.class}\n"
+          @deployer.debug_log "\n\nUnavailable action '#{method_name}' for context #{@deployer.context_name} as #{@deployer.respond_to?(method_name)} and #{@deployer.actions_permitted?(method_name)} for #{@deployer.class}\n"
         end
       end
     end
