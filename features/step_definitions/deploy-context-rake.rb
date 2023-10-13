b802412b69f8be0824de8e303e8a3352ca88600c
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2023-10-13 02:34:32 -0400

    Automatic cucumber commit on branch integrate_cucumber_into_recipe

--- features/step_definitions/deploy-context-rake.rb
+++ features/step_definitions/deploy-context-rake.rb
@@ -4,7 +4,7 @@ Alors('actionne {word} avec rake') do |rake_action|
 end
 
 Alors('bump la version') do
-  stop_test("Bumper la version a échoué", :rake_bump_fail) unless puts("Bump made by rake") || true; system("git version-bump patch")
+  stop_test("Bumper la version a échoué", :rake_bump_fail) unless puts("Bump made by rake") || true #system("git version-bump patch")
 end
 
 Alors('enregistre la version et la date') do
