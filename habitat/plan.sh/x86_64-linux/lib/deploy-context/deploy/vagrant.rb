3a623d44d82f16d2b87a3b0708d5e0d81b5f6f71
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2023-10-15 23:04:01 -0400

    delete duplicate file

--- habitat/plan.sh/x86_64-linux/lib/deploy-context/deploy/vagrant.rb
+++
@@ -1,7 +0,0 @@
-module Context
-  module VagrantHelper
-    def delete_vagrant_box(context, box_name, command_type = :system)
-      context.execute_command(%w(vagrant box remove ) + [box_name], command_type)
-    end
-  end
-end
