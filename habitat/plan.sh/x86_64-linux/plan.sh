c43bdfe4770a3fffe3bbd97a2fa536b26bb77638
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2023-11-07 18:47:08 -0500

    Automatic cucumber commit on branch integrate_cucumber_into_recipe

--- habitat/plan.sh/x86_64-linux/plan.sh
+++ habitat/plan.sh/x86_64-linux/plan.sh
@@ -5,12 +5,6 @@ pkg_maintainer="Jimmy Provencher <jimmy.provencher@hotmail.ca>"
 pkg_license=("MIT")
 pkg_scaffolding=jimbodragon/inspec-profile-deploy-context
 
-show_current_folder() {
-  echo "Showing current folder in $1 step"
-  echo $(pwd)
-  ls -alh
-}
-
 clean_deploy() {
   if [ -d lib ]
   then
@@ -30,32 +24,27 @@ clean_deploy() {
   then
     rmdir bin
   fi
-  find /src --iname *.lock --delete
+  find /src -iname *.lock -delete
 }
 
 do_begin() {
-  show_current_folder 'begin in platform context'
   do_default_begin
 }
 
 do_download() {
-  show_current_folder 'download in platform context'
   do_default_download
 }
 
 do_verify() {
-  show_current_folder 'verify in platform context'
   do_default_verify
 }
 
 do_clean() {
-  show_current_folder 'clean in platform context'
   do_default_clean
   clean_deploy
 }
 
 do_unpack() {
-  show_current_folder 'unpack in platform context'
   do_default_unpack
 
   cp -r /src/libraries lib
@@ -63,13 +52,11 @@ do_unpack() {
 }
 
 do_prepare() {
-  show_current_folder 'prepare in plan context'
   do_default_prepare
   clean_deploy
 }
 
 do_build() {
-  show_current_folder 'build in plan context'
   use_make=false
   if [ "$use_make" == "true" ]
   then
@@ -78,7 +65,6 @@ do_build() {
 }
 
 do_check() {
-  show_current_folder 'check in plan context'
   do_default_exist=false
   if [ "$do_default_exist" == "true" ]
   then
@@ -87,7 +73,6 @@ do_check() {
 }
 
 do_install() {
-  show_current_folder 'install in plan context'
   do_rules_ready=false
   if [ "$do_rules_ready" == "true" ]
   then
@@ -99,7 +84,6 @@ do_install() {
 }
 
 do_strip() {
-  show_current_folder 'strip in platform context'
   do_default_strip
   clean_deploy
 }
--- spec/habitat/plan.sh
+++ spec/habitat/plan.sh
@@ -5,51 +5,65 @@ pkg_svc_user=root
 pkg_license='Apache-2.0'
 pkg_scaffolding="chef/scaffolding-chef-inspec"
 
+show_current_folder() {
+  echo "Showing current folder in $1 step"
+  echo $(pwd)
+  ls -alh
+}
+
 do_begin() {
+  show_current_folder 'begin in platform context'
   do_default_begin
 }
 
 do_download() {
+  show_current_folder 'download in platform context'
   do_default_download
 }
 
 do_verify() {
+  show_current_folder 'verify in platform context'
   do_default_verify
 }
 
 do_clean() {
+  show_current_folder 'clean in platform context'
   do_default_clean
 }
 
 do_unpack() {
+  show_current_folder 'unpack in platform context'
   do_default_unpack
 }
 
 do_prepare() {
+  show_current_folder 'prepare in plan context'
   do_default_prepare
 }
 
 do_build() {
+  show_current_folder 'build in plan context'
   do_default_build
 }
 
 do_check() {
+  show_current_folder 'check in plan context'
   # do_default_check
-  echo "Locating somewhere"
-  echo $(pwd)
-  echo "Il faut bien commencer à quelque part de très loin"
 }
 
 do_install() {
+  show_current_folder 'install in plan context'
   do_default_install
   cp -r /src/lib/ $PREFIX
   ls -alh $PREFIX
 }
 
 do_strip() {
+  show_current_folder 'strip in platform context'
   do_default_strip
 }
 
 do_end() {
+  show_current_folder 'end in platform context'
   do_default_end
 }
