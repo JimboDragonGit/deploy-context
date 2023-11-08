635d21efc1d40757f6696c9ae46dc5d616da7090
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2023-11-07 21:44:04 -0500

    Automatic cucumber commit on branch integrate_cucumber_into_recipe

--- lib/scaffolding.sh
+++ lib/scaffolding.sh
@@ -1,2 +1,20 @@
 
 echo "Executing a scaffolding script for deploy-context"
+
+
+show_folder() {
+  echo "Showing current folder in $1 step"
+  echo $2
+  ls -alh $2
+}
+
+show_current_folder() {
+  show_folder "$1" "$(pwd)"
+}
+
+show_context_folder() {
+  show_current_folder "$1 in $2 context"
+  show_folder "$1 in $2 context" '/hab/cache/src'
+}
+
+show_current_folder 'check in scaffolding context'
