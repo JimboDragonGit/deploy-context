8f66d42f5978b62cf34a87fd11f1d6c36733ba8d
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2023-11-09 00:39:27 -0500

    Automatic cucumber commit on branch integrate_cucumber_into_recipe

--- compliance/profiles/deploy-context/controls/deploy-process.rb
+++ compliance/profiles/deploy-context/controls/deploy-process.rb
@@ -10,51 +10,41 @@ control 'deployment-process-01' do
 
   describe command('knife deploy context cucumber git') do
     its('exit_status') { should eq 0 }
-    it { should exist }
   end
 
   describe command('knife deploy context cucumber knife') do
     its('exit_status') { should eq 0 }
-    it { should exist }
   end
 
   describe command('knife deploy context cucumber kitchen planning') do
     its('exit_status') { should eq 0 }
-    it { should exist }
   end
 
   describe command('knife deploy context cucumber kitchen execution') do
     its('exit_status') { should eq 0 }
-    it { should exist }
   end
 
   describe command('knife deploy context cucumber habitat') do
     its('exit_status') { should eq 0 }
-    it { should exist }
   end
 
   describe command('knife deploy context cucumber rake') do
     its('exit_status') { should eq 0 }
-    it { should exist }
   end
 
   describe command('knife deploy context cucumber supermarket') do
     its('exit_status') { should eq 0 }
-    it { should exist }
   end
 
   describe command('knife deploy context cucumber install') do
     its('exit_status') { should eq 0 }
-    it { should exist }
   end
 
   describe command('knife deploy context cucumber compliance'), :skip do
     its('exit_status') { should eq 0 }
-    it { should exist }
   end
 
   describe command('knife deploy context cucumber git') do
     its('exit_status') { should eq 0 }
-    it { should exist }
   end
 end
