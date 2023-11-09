0240ed93173dea0534993c1b9f2bced47c2fe290
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2023-11-09 05:46:09 -0500

    Automatic cucumber commit on branch integrate_cucumber_into_recipe

--- libraries/deploy-context/controls/command.rb
+++ libraries/deploy-context/controls/command.rb
@@ -2,16 +2,17 @@
 
 # The Chef InSpec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec/resources/
-
-require_relative '../../../../libraries/deploy-context'
-
-extend Context::InspecHelpers::DeployProcess
-
-title 'deploy-context cucumber status control'
-
-%w(kitchen knife habitat inspec compliance rake install supermarket ).each do |app_name|
-  initial_counter = 0
-  control_app(app_name, initial_counter, 'knife')
-  control_app(app_name, initial_counter, 'cucumber')
-  control_app(app_name, initial_counter, 'inspec')
+module Context
+  module ControlsHelpers
+    module CommandHelper
+      def control_deployer_command
+        %w(kitchen knife habitat inspec compliance rake install supermarket ).each do |app_name|
+          initial_counter = 0
+          control_app(app_name, initial_counter, 'knife')
+          control_app(app_name, initial_counter, 'cucumber')
+          control_app(app_name, initial_counter, 'inspec')
+        end
+      end
+    end
+  end
 end
