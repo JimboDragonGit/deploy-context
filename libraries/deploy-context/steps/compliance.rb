2e175422d2f8274abd58306dca68d2393e3646df
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2023-11-10 06:27:41 -0500

    Automatic cucumber commit on branch integrate_cucumber_into_recipe

--- libraries/deploy-context/steps/compliance.rb
+++ libraries/deploy-context/steps/compliance.rb
@@ -4,12 +4,12 @@ module Context
     module ComplianceSteps
       def when_report_succeeded(context_suite)
         extend_command = ["#{context_suite.rapport_name}_json"]
-        if context_suite.rapport_name.include?('kitchen')
-          is_step_kitchen_command = true
+        kitchen_command_ok = if context_suite.rapport_name.include?('kitchen')
           extend_command << context_suite.specific_step
+          false
         end
 
-        stop_test("La commande kitchen ne contien pas d'étape '#{extend_command}'", :no_kitchen_step) if is_step_kitchen_command
+        stop_test("La commande kitchen ne contien pas d'étape '#{extend_command}'", :no_kitchen_step) if kitchen_command_ok
         stop_test("Cucumber Report #{context_suite.rapport_name} is unavailable", :no_report) unless execute_command(%w(knife deploy context cucumber) + extend_command)
 
         results = JSON.parse(::File.read(::File.join('logs/json', "#{context_suite.rapport_name}_features_report.json")))
